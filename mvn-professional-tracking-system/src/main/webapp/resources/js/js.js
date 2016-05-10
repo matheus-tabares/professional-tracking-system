6b613dd1090d55404fc262dc204c40f3e2c8142b
Igor correa <cigor80@gmail.com>
2016-05-10 17:22:59 -0300

    Css e Mobile  testes

---
+++ mvn-professional-tracking-system/src/main/webapp/resources/js/js.js
@@ -0,0 +1,10 @@
+alert('oi');
+$(window).resize(function () {
+
+                    if ($('#conteudo-principal').width() <= 320) {
+
+                        alert('alerta');
+
+                    }
+
+                });
