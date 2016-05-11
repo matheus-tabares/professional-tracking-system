1681855c65b4dd688a5522de0c36bf5f8ebbf3bc
Igor correa <cigor80@gmail.com>
2016-05-10 23:59:44 -0300

    css

--- mvn-professional-tracking-system/target/mvn-professional-tracking-system-1.0-SNAPSHOT/resources/js/js.js
+++ mvn-professional-tracking-system/target/mvn-professional-tracking-system-1.0-SNAPSHOT/resources/js/js.js
@@ -1,10 +1,1 @@
-alert('oi');
-$(window).resize(function () {
 
-                    if ($('#conteudo-principal').width() <= 320) {
-
-                        alert('alerta');
-
-                    }
-
-                });
