546a5603998e6e8b895aa1d74d575809831d513c
samuelcrs <samuelcrs@hotmail.com>
2016-05-30 10:36:14 -0300

    fotos
    parcial da funcionalidade opload de fotos

---
+++ pts-v2/src/main/java/com/dev2/controller/FileUploadBean.java
@@ -0,0 +1,97 @@
+package com.dev2.controller;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 10070099
+ */
+
+    
+ 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+ 
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+ 
+import org.primefaces.event.FileUploadEvent;
+  
+@ManagedBean(name="fileUploadController")
+public class FileUploadBean {
+   private String destination="C:\\Users\\10070099\\Desktop\\uploadImagem\\";
+   private String selecao;
+ 
+    public void upload(FileUploadEvent event) {  
+        FacesMessage msg = new FacesMessage("Success! ", event.getFile().getFileName() + " is uploaded.");  
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        // Do what you want with the file        
+        try {
+            copyFile(event.getFile().getFileName(), event.getFile().getInputstream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+ 
+    }  
+ 
+    public void copyFile(String fileName, InputStream in) {
+           try {
+              
+              
+                // write the inputStream to a FileOutputStream
+                OutputStream out = new FileOutputStream(new File(getDestination() + fileName));
+                selecao =  out.toString();
+                int read = 0;
+                byte[] bytes = new byte[1024];
+              
+                while ((read = in.read(bytes)) != -1) {
+                    out.write(bytes, 0, read);
+                }
+              
+                in.close();
+                out.flush();
+                out.close();
+              
+                System.out.println("New file created!");
+                } catch (IOException e) {
+                System.out.println(e.getMessage());
+                }
+    }
+
+    /**
+     * @return the destination
+     */
+    public String getDestination() {
+        return destination;
+    }
+
+    /**
+     * @param destination the destination to set
+     */
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    /**
+     * @return the selecao
+     */
+    public String getSelecao() {
+        return selecao;
+    }
+
+    /**
+     * @param selecao the selecao to set
+     */
+    public void setSelecao(String selecao) {
+        this.selecao = selecao;
+    }
+}
+
