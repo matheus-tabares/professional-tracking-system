25f44fa824e09647efb2611ca13afd01b04d4337
Igor correa <cigor80@gmail.com>
2016-06-09 22:10:37 -0300

    Correção Home
    pequena correção na funcionalidade mapa

--- pts-v2/src/main/webapp/resources/js/mapas.js
+++ pts-v2/src/main/webapp/resources/js/mapas.js
@@ -8,16 +8,22 @@ $(document).ready(function () {
     map = new GMaps({
         el: '#map',
         lat: -12.043333,
-        lng: -77.028333,
-        zoomControl: true,
-        zoomControlOpt: {
-            style: 'SMALL',
-            position: 'TOP_LEFT'
-        },
-        panControl: false,
-        streetViewControl: false,
-        mapTypeControl: false,
-        overviewMapControl: false
+        lng: -77.028333
+    });
+    $('#geocoding_form').submit(function (e) {
+        e.preventDefault();
+        GMaps.geocode({
+            address: $('#address').val().trim(),
+            callback: function (results, status) {
+                if (status == 'OK') {
+                    var latlng = results[0].geometry.location;
+                    map.setCenter(latlng.lat(), latlng.lng());
+                    map.addMarker({
+                        lat: latlng.lat(),
+                        lng: latlng.lng()
+                    });
+                }
+            }
+        });
     });
 });
-
